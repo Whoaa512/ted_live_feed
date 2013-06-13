8a7be32ca5bd5e9d54f811ec224b8d46cae240b1
Whoaa512 <whoaa512@gmail.com>
2013-06-13 08:47:34 -0700

    Updated model with default values

--- models/feed.js
+++ models/feed.js
@@ -1,5 +1,19 @@
 var FeedItem = Backbone.Model.extend({
 
+  initialize: function(params) {
+    params = params || {};
+
+    // Create easy access to pertinent data
+    this.thumbnail = params.mediaGroups[0].contents[0].thumbnails[0].url;
+    this.videoLink = params.mediaGroups[0].contents[0].url;
+    this.title = params.title;
+    this.tedLink = params.link;
+    this.publishedDate = params.publishedDate;
+    this.category = params.category;
+    this.description = params.content;
+    this.teaser = params.contentSnippet;
+  }
+
 });
 
 var FeedList = Backbone.Collection.extend({
