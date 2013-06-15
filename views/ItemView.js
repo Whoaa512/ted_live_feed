a2990380ab6dd7428a9f2433966366a1c42d5bf8
Whoaa512 <whoaa512@gmail.com>
2013-06-15 15:27:25 -0700

    ItemView defined w/ template

--- views/ItemView.js
+++ views/ItemView.js
@@ -1,3 +1,13 @@
 var ItemView = Backbone.View.extend({
-  initialize: function() {}
+  className: 'one_third',
+
+  template: _.template(
+    "<img class='thumbnail-main' src='<%= thumbnail %>'/>"+
+    "<p><%= title %> - <%= publishedDate %></p>"+
+    "<p><%= teaser %></p>"),
+
+  render: function() {
+    return this.$el.append(this.template(this.model.attributes));
+  }
+
 });
