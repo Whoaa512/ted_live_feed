b3a3e19b32dc2c78dfe30bf15bfbe709bbd79975
Whoaa512 <whoaa512@gmail.com>
2013-06-15 14:46:45 -0700

    Created namespace for whole app

--- setup.js
+++ setup.js
@@ -1,22 +1,26 @@
-var feedEntries = new FeedList();
+var TEDLiveFeed = {}; // Namespace for app view & model
+
 
 google.load("feeds", "1"); // Load the feed api
 
 function feedLoaded() {
-  var tedFeed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
+  var tedFeed = TEDLiveFeed.feed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
   tedFeed.includeHistoricalEntries();
   tedFeed.setNumEntries(300);
+
+  var feedEntries = new FeedList();
 
   tedFeed.load(function(result) {
     if (!result.error) {
       result.feed.entries.forEach(function(entry, key, list) {
         feedEntries.add(entry);
       });
-    } else { console.log(result.error); }
-
-    var app = new App({feedList: feedEntries});
-    var appView = new AppView({model: app});
-
+      var app = TEDLiveFeed.app = new App({feedList: feedEntries});
+      TEDLiveFeed.appView = new AppView({model: app});
+    } else {
+      console.log(result.error);
+      throw new Error('Feed wasn\'t loaded properly.');
+    }
   });
 }
 
