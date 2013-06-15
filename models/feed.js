6749684aff9946ace8f9ee704d47ceaafad87fea
Whoaa512 <whoaa512@gmail.com>
2013-06-15 16:50:56 -0700

    Stores metadata w/ model.set

--- models/feed.js
+++ models/feed.js
@@ -4,14 +4,14 @@ var FeedItem = Backbone.Model.extend({
     params = params || {};
 
     // Create easy access to pertinent data
-    this.thumbnail = params.mediaGroups[0].contents[0].thumbnails[0].url;
-    this.videoLink = params.mediaGroups[0].contents[0].url;
-    this.title = params.title;
-    this.tedLink = params.link;
-    this.publishedDate = params.publishedDate;
-    this.category = params.categories[0];
-    this.description = params.content;
-    this.teaser = params.contentSnippet;
+    this.set('thumbnail',  = params.mediaGroups[0].contents[0].thumbnails[0].url);
+    this.set('videoLink',  = params.mediaGroups[0].contents[0].url);
+    this.set('title',  = params.title);
+    this.set('tedLink',  = params.link);
+    this.set('publishedDate',  = params.publishedDate);
+    this.set('category',  = params.categories[0]);
+    this.set('description',  = params.content);
+    this.set('teaser',  = params.contentSnippet);
   }
 
 });
