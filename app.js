8251218c94940a6675deee1cc1432ba5aa452295
Whoaa512 <whoaa512@gmail.com>
2013-06-15 13:10:51 -0700

    Created app model definition; removed api tests

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
-google.load("feeds", "1");
-
-
-function initialize() {
-  var feed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
-  feed.setNumEntries(4);
-  feed.load(function(result) {
-    if (!result.error) {
-      var container = document.getElementById("feed");
-
-      result.feed.entries.forEach(function(entry, key, list) {
-
-        // Create `<img>` & set thumbnail src
-        var thumbnail = document.createElement('img');
-        thumbnail.setAttribute('src', entry.mediaGroups[0].contents[0].thumbnails[0].url);
-        thumbnail.setAttribute('class', 'thumbnail-main');
-
-        // Create container feed entry
-        var entryDiv = document.createElement("div");
-        entryDiv.appendChild(thumbnail);
-        entryDiv.appendChild(document.createTextNode(entry.title));
-        container.appendChild(entryDiv);
-      });
-    }
-  });
-}
-
-google.setOnLoadCallback(initialize);
+var App = Backbone.Model.extend({
+  initialize: function(params) {
+    // Use the hydrated collection or create one
+    this.feedList = params.feedList || new FeedList();
+  }
+});
