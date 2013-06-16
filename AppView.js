d3ce1556f77429c00b75e30cbeca34d86c050641
Whoaa512 <whoaa512@gmail.com>
2013-06-15 17:56:23 -0700

    Implemented main app view render function

--- AppView.js
+++ AppView.js
@@ -1,3 +1,24 @@
 var AppView = Backbone.View.extend({
-  initialize: function() {}
+  el: $('div#feed'),
+
+  initialize: function() {
+    _.bindAll(this, 'render');
+    this.render();
+  },
+
+  render: function() {
+    var feedList = this.model.get('feedList');
+    return this.$el.html(
+      feedList.map(function(item, index) {
+        if ((index+1) % 3 === 0) {
+          console.log("last");
+          var lastThird = new ItemView({model: item});
+          lastThird.$el.addClass('last');
+          return lastThird.render();
+        } else {
+          return new ItemView({model: item}).render();
+        }
+      })
+    );
+  }
 });
