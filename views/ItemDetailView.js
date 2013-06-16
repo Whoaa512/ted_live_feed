12eb555c6008c8a49a07b7f6140bf74df40a1e6d
Whoaa512 <whoaa512@gmail.com>
2013-06-16 09:37:35 -0700

    Added ItemDetailView template

--- views/ItemDetailView.js
+++ views/ItemDetailView.js
@@ -1,3 +1,12 @@
 var ItemDetailView = Backbone.View.extend({
-  initialize: function() {}
+  template: _.template(
+    "<p><img class='thumbnail-main' src='<%= thumbnail %>'/>"+
+    "<h3><%= title %></h3></p>"+
+    "<p><%= publishedDate %> - <%= category %></p>"+
+    "<p><%= tedLink %></p>"+
+    "<p><%= description %></p>"),
+
+  render: function() {
+    return this.$el.html(this.template(this.model.attributes));
+  }
 });
