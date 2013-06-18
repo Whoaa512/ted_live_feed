41abe904d8c7cf38af6bd5eaf53b2b79281b2a3d
Whoaa512 <whoaa512@gmail.com>
2013-06-17 20:32:32 -0700

    Update ItemDetailView to use bootstrap modal

--- views/ItemDetailView.js
+++ views/ItemDetailView.js
@@ -1,10 +1,18 @@
 var ItemDetailView = Backbone.View.extend({
   template: _.template(
-    "<p><img class='thumbnail-main' src='<%= thumbnail %>'/>"+
-    "<h3><%= title %></h3></p>"+
-    "<p><%= publishedDate %> - <%= category %></p>"+
-    "<p><%= tedLink %></p>"+
-    "<p><%= description %></p>"
+    "<div class='modal-header'>"+
+      "<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>"+
+      "<p><img class='thumbnail-main' src='<%= thumbnail %>'/>"+
+      "<h3><%= title %></h3></p>"+
+      "<p><%= publishedDate %> - <%= category %></p>"+
+      "<p><%= tedLink %></p>"+
+    "</div>"+
+    "<div class='modal-body'>"+
+      "<p><%= description %></p>"+
+    "</div>"+
+    "<div class='modal-footer'>"+
+      "<a href='#' class='btn btn-primary'>Close</a>"+
+    "</div>"
   ),
 
   render: function() {
